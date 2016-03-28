1bd5ed6d383499d11071206620100f48a7ae050f
mclifton94 <m.clifton94@live.com>
2016-03-28 14:24:55 -0400

    Initial Commit

---
+++ FinalProjectDesPats/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProjectDesPats
+//
+//  Created by Mark Clifton on 3/28/16.
+//  Copyright © 2016 designPatterns. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
